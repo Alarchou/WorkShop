0fc903df591c083c4f3cfb89505d1352359871b4
Deerah4 <sarah.brgb@gmail.com>
2025-11-21 02:11:29 +0100

    ajout son et video

---
+++ Assets/Scripts/MonsterScreamerr.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MonsterScreamerTrigger : MonoBehaviour
+{
+    [SerializeField] private string organTag = "Organ";              // Tag des organes
+    [SerializeField] private string screamerSceneName = "ScreamerScene"; // Nom de la scène screamer
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(organTag))
+        {
+            // Quand un organ touche le monstre → on charge la scène Screamer
+            SceneManager.LoadScene(screamerSceneName);
+        }
+    }
+}
